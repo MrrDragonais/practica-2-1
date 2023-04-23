2d0ec728f028fb5e886549a1667857d0b782a165
nikit <nikit@DESKTOP-5363RTP>
2023-04-23 15:25:02 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream> 
+#include<cstdio>
+class Road {
+public:
+	double length;
+	int width;
+	Road();
+};
+Road::Road() {
+	length = 5;
+	width = 555;
+};
+int main() {
+	using namespace std;
+	cout << "Test v0.1: OK.\n";
+	Road road;
+	cout << "before access: \n";
+	cout << "length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+	road.length = 444.4;
+	road.width = 4;
+	cout << "\nAfter access: \n";
+	cout << "length: " << road.length << endl;
+	cout << "Width: " << road.width << endl;
+}
